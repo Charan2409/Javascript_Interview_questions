5f4566b062d61c9bafd85133d2353a3754329025
Sai charan bandi <108537114+Charan2409@users.noreply.github.com>
2026-01-06 20:38:07 +0530

    Update ArrayPrototypes.js

--- ArrayPrototypes.js
+++ ArrayPrototypes.js
@@ -3,13 +3,13 @@
 //   filtered down to just the elements from the given array that pass the test implemented by the provided function.
 
 // Solution:
-const arr = [1,2,3,4,5]
+var arr = [1,2,3,4,5]
 
 Array.prototype.myFilter = function(callbackFn, thisArg){
-    let len = this.length;
-    const result = []
+    var len = this.length;
+    var result = []
 
-    for(let i=0; i<len; i++){
+    for(var i=0; i<len; i++){
          if(callbackFn.call(thisArg, this[i], i, this)){
         result.push(this[i])
     }
@@ -18,6 +18,41 @@ Array.prototype.myFilter = function(callbackFn, thisArg){
         return result
 }
 
-const myArr  = arr.myFilter((val) => val > 3)
+var myArr  = arr.myFilter((val) => val > 3)
 console.log(myArr) //[4.5]
+
+//Array.prototype.map
+const arr = [1,2,3,4,5]
+
+Array.prototype.myMap = function(callbackFn, thisArg){
+    var len = this.length
+    var result = []
+
+    for(var i =0;i<len;i++){
+       result.push(callbackFn.call(thisArg, this[i], i, this))
+    }
+    return result
+}
+
+var myArr  = arr.myMap((val) => val * 3)
+console.log(myArr)
+
+
+//Array.prototype.reduce
+const arr = [1,2,3,4,5]
+
+Array.prototype.myReduce = function(callbackFn, initialValue){
+    var accumulator = initialValue;
+    for(var i = 0; i < this.length; i++){
+        accumulator = accumulator ? callbackFn(accumulator, this[i], i, this) : this[0]
+    }
+    return accumulator
+    
+}
+
+const myArr  = arr.myReduce((acc, curr) => {
+    return acc + curr
+}, 0)
+console.log(myArr)
+
 
